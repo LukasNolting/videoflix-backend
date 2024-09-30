63a401cb130220d5905e7cabee668f15c75679f4
Lukas Nolting <lukas.nolting@icloud.com>
2024-09-30 20:54:49 +0200

    redis & rq worker

--- videoflix_app/views.py
+++ videoflix_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.core.cache.backends.base import DEFAULT_TIMEOUT 
+from django.views.decorators.cache import cache_page 
+from django.conf import settings
+CACHETTL = getattr(settings, 'CACHETTL', DEFAULT_TIMEOUT)
+
+
+@cache_page(CACHETTL)
+def test():
+    print("test")
