bb88a7ef5aa1a69df38698f7668bc32f8b6a934c
Lukas Nolting <lukas.nolting@icloud.com>
2024-10-06 19:00:34 +0200

    signup

--- videoflix_app/urls.py
+++ videoflix_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from videoflix_app.views import LoginView, VideoView
+from videoflix_app.views import LoginView, RegisterView, VideoView
 
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('videos/', VideoView.as_view(), name='video-detail'),
+    path('signup/', RegisterView.as_view(), name='signup'),
 ]
