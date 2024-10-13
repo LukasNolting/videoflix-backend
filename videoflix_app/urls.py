d76d41444b131418822291b29a7b2e3ffffa4a0e
Lukas Nolting <lukas.nolting@icloud.com>
2024-10-13 17:06:05 +0200

    favorites api

--- videoflix_app/urls.py
+++ videoflix_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from videoflix_app.views import LoginView, RegisterView, VideoView
 from videoflix_app.views import activate_user
-from .views import RequestPasswordReset, PasswordResetView
+from .views import RequestPasswordReset, PasswordResetView, favorite_videos
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('activate/<uidb64>/<token>/', activate_user, name='activate_user'),
     path('password-reset/', RequestPasswordReset.as_view(), name='password-reset'),
-    path('password-reset/<token>/', PasswordResetView.as_view(), name='password-reset-token')
+    path('password-reset/<token>/', PasswordResetView.as_view(), name='password-reset-token'),
+    path('favorite/', favorite_videos, name='toggle_favorite'),
 ]
