5f15a4cb190d876944b6ea040ee07cefe484b5e1
Lukas Nolting <lukas.nolting@icloud.com>
2024-10-21 12:41:05 +0200

    admin interface

--- videoflix_app/admin.py
+++ videoflix_app/admin.py
@@ -2,31 +2,64 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from django.utils.html import format_html
-from rest_framework.authtoken.models import Token
 
-from .models import Video
+from .models import Video, User, PasswordReset, UserFavoriteVideo, UserContinueWatchVideo
 
+
+# Resource für Video-Modell (für den Import/Export)
 class VideoResource(resources.ModelResource):
-
     class Meta:
         model = Video
-        
+
+
+# Admin-Konfiguration für das Video-Modell
 @admin.register(Video)
 class VideoAdmin(ImportExportModelAdmin):
-    pass
+    resource_class = VideoResource
+    list_display = ('title', 'category', 'created_at', 'video_thumbnail')
+    list_filter = ('category', 'created_at')
+    search_fields = ('title', 'description')
+
+    # Thumbnail in der Admin-Liste anzeigen
+    def video_thumbnail(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" width="100" height="100" />'.format(obj.thumbnail.url))
+        return "No Thumbnail"
+    video_thumbnail.short_description = 'Thumbnail'
 
 
-# class CustomUserAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'username', 'email', 'first_name', 'last_name',
-#         'is_staff', 'is_active', 'last_login', 'date_joined',
-#         'is_superuser', 'remember', 'get_auth_token'
-#     )
-#     list_filter = ('username', 'email', 'first_name', 'last_name')
-#     search_fields = ('username', 'email', 'first_name', 'last_name')
-#     ordering = ('username', 'email', 'first_name', 'last_name')
-#     fieldsets = (
-#         ('Personal Information', {'fields': ('username', 'email', 'first_name', 'last_name')}),
-#     )
+# Admin-Konfiguration für das User-Modell
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_active', 'remember', 'is_staff')
+    list_filter = ('is_active', 'remember', 'is_staff')
+    search_fields = ('email', 'username', 'is_active', 'remember', 'is_staff')
+    ordering = ('email',)
 
-# admin.site.register(CustomUserAdmin)
+
+# Admin-Konfiguration für das PasswordReset-Modell
+@admin.register(PasswordReset)
+class PasswordResetAdmin(admin.ModelAdmin):
+    list_display = ('email', 'token', 'created_at')
+    search_fields = ('email', 'token')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
+
+# Admin-Konfiguration für das UserFavoriteVideo-Modell
+@admin.register(UserFavoriteVideo)
+class UserFavoriteVideoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'is_favorite', 'created_at')
+    list_filter = ('is_favorite', 'created_at')
+    search_fields = ('user__email', 'video__title')
+    ordering = ('-created_at',)
+
+
+# Admin-Konfiguration für das UserContinueWatchVideo-Modell
+@admin.register(UserContinueWatchVideo)
+class UserContinueWatchVideoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'timestamp', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__email', 'video__title')
+    ordering = ('-created_at',)
+
